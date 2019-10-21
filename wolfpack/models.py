8e23b4fda8da578f12bccb197f52625628015be4
Andy <f2280c@gmail.com>
2019-10-21 21:17:58 +0800

    added some models

--- wolfpack/models.py
+++ wolfpack/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+# don't need to create primary key as django will create it for us
+class User(models.Model):
+    name = models.CharField()
+
+
+class Project(models.Model):
+    title = models.CharField()
+    description = models.CharField()
+    scrumMaster = models.ForeignKey(User)
