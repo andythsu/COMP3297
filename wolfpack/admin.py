6e718966c1ee02e1af31d42d0cd06c2a8e6b0a6e
YAULAISHUN <yaulaishun@yahoo.com.hk>
2019-10-22 18:51:52 +0800

    Shawn

--- wolfpack/admin.py
+++ wolfpack/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import ProductOwner,ScrumMaster,Developer,Project,PBI
 
+
+admin.site.register(ProductOwner)
+admin.site.register(ScrumMaster)
+admin.site.register(Developer)
+admin.site.register(Project)
+admin.site.register(PBI)
 # Register your models here.
--- wolfpack/models.py
+++ wolfpack/models.py
@@ -1,14 +1,33 @@
 from django.db import models
 
-
-# Create your models here.
-# don't need to create primary key as django will create it for us
-
 class User(models.Model):
-    name = models.CharField()
+    userID = models.IntegerField(primary_key=True)
+    name   = models.CharField(max_length=20)
+    role   = models.CharField(max_length=20)
+    class Meta:
+        abstract = True
 
+class ProductOwner(User):
+    projectID  = models.ForeignKey('Project', on_delete=models.PROTECT, blank=False, null=False)
+
+class ScrumMaster(User):
+    projectID  = models.ForeignKey('Project', on_delete=models.PROTECT, blank=True, null=True)
+
+class Developer(User):
+    projectID  = models.ForeignKey('Project', on_delete=models.PROTECT, blank=True, null=True)
 
 class Project(models.Model):
-    title = models.CharField()
-    description = models.CharField()
-    scrumMaster = models.ForeignKey(User)
+    projectID    = models.IntegerField(primary_key=True)
+    title        = models.CharField(max_length=50)
+    description  = models.TextField(blank=True,null=True)
+    scrum_master = models.IntegerField()
+
+class PBI(models.Model):
+    ID         = models.IntegerField(primary_key=True)
+    size       = models.IntegerField()
+    priority   = models.IntegerField()
+    status     = models.CharField(max_length=10)
+    user_story = models.TextField()
+    projectID  = models.ForeignKey('Project', on_delete=models.PROTECT, blank=False, null=False)
+    # This attribute is for further implementation.
+    # sprintID = models.ForeignKey('Sprint', blank=True, null=True)
