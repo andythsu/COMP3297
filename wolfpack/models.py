409f3ffe36dfb4a2b391a221a529a5d4fcce036f
YAULAISHUN <yaulaishun@yahoo.com.hk>
2019-10-25 12:57:07 +0800

    fix the User bug

--- wolfpack/models.py
+++ wolfpack/models.py
@@ -1,29 +1,29 @@
 from django.db import models
 
-# class User(models.Model):
-#     name = models.CharField(max_length=20)
-#     role = models.CharField(max_length=20)
-#
-#     class Meta:
-#         abstract = True
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    role = models.CharField(max_length=20)
+
+    class Meta:
+        abstract = True
+
+
+class ProductOwner(User):
+    projectId = models.ForeignKey('Project', on_delete=models.DO_NOTHING, blank=False, null=False)
+
+
+class ScrumMaster(User):
+    projectId = models.ForeignKey('Project', on_delete=models.DO_NOTHING, blank=True, null=True)
+
+
+class Developer(User):
+    projectId = models.ForeignKey('Project', on_delete=models.DO_NOTHING, blank=True, null=True)
 
 
 class Project(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
-    # scrumMaster = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
-
-
-# class ProductOwner(User):
-#     projectId = models.ForeignKey(Project, on_delete=models.DO_NOTHING, blank=False, null=False)
-#
-#
-# class ScrumMaster(User):
-#     projectId = models.ForeignKey(Project, on_delete=models.DO_NOTHING, blank=True, null=True)
-#
-#
-# class Developer(User):
-#     projectId = models.ForeignKey(Project, on_delete=models.DO_NOTHING, blank=True, null=True)
+    scrumMaster = models.ForeignKey('ScrumMaster', on_delete=models.DO_NOTHING, blank=True, null=True)
 
 
 class ProductBacklogItem(models.Model):
