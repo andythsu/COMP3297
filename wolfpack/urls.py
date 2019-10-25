e1b95201a472cca3392601b5368507dac2696946
Sam <shermancheungapple@gmail.com>
2019-10-25 16:04:01 +0800

    Added two lists, project creation page
    All linked, pbis will refer to a project as well.

--- wolfpack/urls.py
+++ wolfpack/urls.py
@@ -2,16 +2,21 @@ from django.urls import path
 
 from . import views
 from .dao import ProductBacklogItemDao
+from .dao import ProjectDao
 
 app_name = 'wolfpack'
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('insert', views.insert, name='insert'),
-    path('details/<int:id>', views.details, name='detail'),
-    path('delete/<int:id>', views.delete, name='delete'),
-    path('update/<int:id>', views.update, name='update'),
+    path('', views.index_project, name='index'),
+    path('project/insert/<int:id>', views.insert, name='insert'),
+    path('index_project', views.index_project, name='index_project'),
+    path('add_project', views.add_project, name='add_project'),
+    path('delete_project/<int:id>', views.delete_project, name='delete_project'),
+    path('project/details/<int:id>', views.details, name='detail'),
+    path('project/<int:id>', views.project, name='project'),
+    path('project/delete/<int:id>', views.delete, name='delete'),
+    path('project/update/<int:id>', views.update, name='update'),
 
     # button actions
 ]
--- wolfpack/views.py
+++ wolfpack/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 
 from .models import ProductBacklogItem
+from .models import Project
 
 
 def index(request):
@@ -15,20 +16,21 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-def insert(request):
+def insert(request, id):
     if (request.method == 'POST'):
         pbi = ProductBacklogItem(
             size=request.POST['size'],
             priority=request.POST['priority'],
             status=request.POST['status'],
             userStory=request.POST['userStory'],
-            projectId=request.POST['projectId'],
+            projectId=id,
         )
         pbi.save()
         messages.success(request, 'Product Added : %s' % pbi.projectId)
-        return redirect(reverse('wolfpack:index'))
+        return redirect(reverse('wolfpack:project', args=[id]))
     else:
         context = {
+            'id': id
         }
         return render(request, 'add.html', context)
 
@@ -48,10 +50,9 @@ def update(request, id):
         pbi.priority = request.POST['priority']
         pbi.status = request.POST['status']
         pbi.userStory = request.POST['userStory']
-        pbi.projectId = request.POST['projectId']
         pbi.save()
         messages.success(request, 'Product Updated : %s' % pbi.projectId)
-        return redirect(reverse('wolfpack:detail', args=[id]))
+        return redirect(reverse('wolfpack:project', args=[pbi.projectId]))
 
     else:
         context = {
@@ -60,10 +61,53 @@ def update(request, id):
     return render(request, 'edit.html', context)
 
 
+
 def delete(request, id):
-    if (request.method == 'POST'):
+    if request.method == 'POST':
         pbi = get_object_or_404(ProductBacklogItem, pk=id)
         messages.success(request, 'Product Deleted : %s' % pbi.projectId)
+        pid = pbi.projectId
         pbi.delete()
 
-    return redirect(reverse('wolfpack:index'))
+    return redirect(reverse('wolfpack:project', args=[pid]))
+
+
+def add_project(request):
+    if (request.method == 'POST'):
+        project = Project(
+            title=request.POST['title'],
+            description=request.POST['description'],
+            scrumMaster=request.POST['scrumMaster'],
+        )
+        project.save()
+        messages.success(request, 'Project Added : %s' % project.title)
+        return redirect(reverse('wolfpack:index_project'))
+    else:
+        return render(request, 'add_project.html')
+
+
+def index_project(request):
+    pro = Project.objects.all()[:10]
+    context = {
+        'projects': pro
+    }
+    return render(request, 'index_project.html', context)
+
+def project(request, id):
+    pro = get_object_or_404(Project, pk=id)
+    pbi2 = ProductBacklogItem.objects.filter(projectId=id, status="not done")
+    pbi = ProductBacklogItem.objects.filter(projectId=id, status="done")
+    context = {
+        'pro': pro,
+        'pbis': pbi,
+        'pbis2': pbi2
+    }
+    return render(request, 'project.html', context)
+
+def delete_project(request, id):
+    if request.method == 'POST':
+        pro = get_object_or_404(Project, pk=id)
+        messages.success(request, 'Project Deleted : %s' % pro.title)
+        pro.delete()
+    return redirect(reverse('wolfpack:index_project'))
+
