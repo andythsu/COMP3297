c2d2f9c0051a2313e030ea32823e716606a3d151
YAULAISHUN <yaulaishun@yahoo.com.hk>
2019-11-05 21:06:55 +0800

    Added all sprint page

--- wolfpack/SprintBacklogViews.py
+++ wolfpack/SprintBacklogViews.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
-from .dao import SprintBacklogDao
+from .dao import ProjectDao, SprintBacklogDao
+
+
+def index(request, proId):
+    pro = ProjectDao.getProjectById(proId)
+    sprints = SprintBacklogDao.getAllSprints(proId)
+    sprintList = []
+    for sprint in sprints:
+        sprintList.append({
+            'sprint': sprint,
+        })
+    context = {
+        'pro': pro,
+        'sprints': sprintList
+    }
+    return render(request, 'SprintIndex.html', context)
 
 
 def insert(request, proId):
@@ -14,7 +29,7 @@ def insert(request, proId):
             projectId=proId
         )
         messages.success(request, 'sprint backlog added : %s' % sprintBacklogId)
-        return redirect(reverse('wolfpack:add_sprint', args=[proId]))
+        return redirect(reverse('wolfpack:index_sprint', args=[proId]))
     else:
         context = {
             'projectId': proId
